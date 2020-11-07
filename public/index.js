5cc246d30c588a40423a085b829dc616a395a0a5
Joey Dang <jodang@ucsd.edu>
2020-11-07 01:48:32 -0800

    Add image and style

---
+++ public/index.js
@@ -0,0 +1,15 @@
+var element = document.getElementById("form");
+element.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const zipcode = event.target.elements.zipcode.value;
+  console.log(zipcode);
+  fetch(`http://localhost:3000/findWeather?zipcode=${zipcode}`, {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json' },
+  }).then(response => response.json())
+  .then(json => {
+    document.getElementById("temperature").innerHTML = json.main.temp + '°';
+    document.getElementById("description").innerHTML = json.weather[0].description;
+  })
+  .catch(err => console.error(err))
+});
